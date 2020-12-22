c275790ec7c2d40d25927117fd0461fb475663d6
Leonard Vaney <leonard.vaney@epfl.ch>
2020-12-22 16:22:52 +0100

    arrangement du main pour la suite + ajout de variables

--- source/main.c
+++ source/main.c
@@ -22,53 +22,72 @@ typedef struct {
 
 typedef struct {
 	int difficulte; //0, 1, 2 (défini la difficulté du niveau)
-	int vitesse; //peut aller vers l'infini (défini
-} challenge;
+	int vitesse; //peut aller vers l'infini (défini le temps donné au joueur
+} game_challenge;
 
 int main(void) {
+
+	//à déf à l'extérieur (je sais pas encore)
+	bool recharge_du_jeu = false;
+	bool lancement_du_jeu = false;
+
+	game_status status;
+	status.score = 0;
+	status.vie_restante = VIE_MAX;
+
+	game_challenge challenge;
+	challenge.difficulte = 0;
+	challenge.vitesse = 0;
+
 	Graphics_setup_main();
 	Draw_default_screen();
 	
 	mmInitDefaultMem((mm_addr)soundbank_bin);
-
 	mmLoad(MOD_TEST1);
-
 	mmStart(MOD_TEST1, MM_PLAY_LOOP);
 
 	/*consoleDemoInit();
     printf("\nTemplate nds\n"); */
 
+	//while général qui contient tout le déroulement du jeu
+	while(1){
 
-	/*
-	 *
-	 *
-	 * ici création du menu (bouton pour lancer le jeu + bg si nécessaire)
-	 *
-	 *
-	 */
+		while(!lancement_du_jeu){
+					/*
+					 *
+					 *
+					 * ici création du menu (bouton pour lancer le jeu + bg si nécessaire)
+					 *
+					 *
+					 */
+				swiWaitForVBlank(); //nécessaire?
+			}
 
+			lancement_du_jeu = false;
 
-	/*
-	 *
-	 *
-	 *
-	 * le jeu est lancé, on entre dans une boucle while qui lance des jeux
-	 *
-	 *
-	 */
+			while(!status.vie_restante){
+					/*
+					 *
+					 *
+					 *
+					 * le jeu est lancé, on entre dans une boucle while qui lance des jeux
+					 *
+					 *
+					 */
+				swiWaitForVBlank(); //nécessaire?
+			}
 
+			while(!recharge_du_jeu){
+					/*
+					 *
+					 *
+					 * le jeu a été perdu, on affiche un écran particulier avec un bouton pour
+					 * revenir au menu
+					 *
+					 *
+					 */
+				swiWaitForVBlank(); //nécessaire?
+			}
+	}
 
-
-	/*
-	 *
-	 *
-	 *
-	 * le jeu a été perdu, on affiche un écran particulier avec un bouton pour
-	 * revenir au menu
-	 *
-	 *
-	 */
-
-    while(1)
-        swiWaitForVBlank();	
 }
